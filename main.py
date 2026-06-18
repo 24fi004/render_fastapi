749f0a4b75225180a2fbd8cb224f58c7f41fb3e7
24fi004 <24fi004@ms.dendai.ac.jp>
2026-06-19 00:33:13 +0900

    Update main.py

--- main.py
+++ main.py
@@ -25,6 +25,16 @@ def omikuji():
     ]
     return omikuji_list[random.randrange(10)]
 
-@app.get("/")
-def read_index():
-    return FileResponse("index.html")
+@app.get("/index")
+def index():
+    html_content = """
+    <html>
+        <head>
+            <title>Some HTML in here</title>
+        </head>
+        <body>
+            <h1>Look ma! HTML!</h1>
+        </body>
+    </html>
+    """
+    return HTMLResponse(content=html_content, status_code=200)
