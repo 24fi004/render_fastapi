27d6371e8ccdd4f27218f8646cfceb177e7e8858
24fi004 <24fi004@ms.dendai.ac.jp>
2026-06-19 00:20:27 +0900

    Refactor omikuji endpoint and update routes

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 from typing import Optional
-
 from fastapi import FastAPI
-
-import random  # randomライブラリを追加
-
 from fastapi.responses import FileResponse
+import random
 
 app = FastAPI()
-
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
@@ -16,21 +12,19 @@ def read_item(item_id: int, q: Optional[str] = None):
 @app.get("/omikuji")
 def omikuji():
     omikuji_list = [
-         "大吉！素晴らしい幸運が舞い込むでしょう。",
-         "中吉！努力が実を結び、良い結果が待っています。",
+        "大吉！素晴らしい幸運が舞い込むでしょう。",
+        "中吉！努力が実を結び、良い結果が待っています。",
         "小吉！ちょっとした幸運があなたの元にやってきます。",
         "吉！安定した幸せな日々が続くでしょう。",
         "半吉",
         "末吉！努力が実り始め、良い方向に進む時期です。",
         "末小吉",
         "凶。悪いことが起こるかもしれませんが、気を引き締めてください。",
-         "小凶。注意が必要な日です。慎重に行動しましょう。",
+        "小凶。注意が必要な日です。慎重に行動しましょう。",
         "大凶。厳しい状況が訪れるかもしれませんが、乗り越えましょう。"
     ]
-
     return omikuji_list[random.randrange(10)]
 
-@app.get("/index")
+@app.get("/")
 def read_index():
     return FileResponse("index.html")
-
